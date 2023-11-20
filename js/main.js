77bf44730af91e1e66680261e320eb2e2301e82f
Luigi-Iorio <luigi0iorio@gmail.com>
2023-11-20 15:14:40 +0100

    feat: struttura dati con info membri del team

---
+++ js/main.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// array info membri del team
+const membriTeam = [
+  {
+    nomeCognome: "Wayne Barnett",
+    ruolo: "Founder & CEO",
+    foto: "wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    nomeCognome: "Angela Caroll",
+    ruolo: "Chief Editor",
+    foto: "angela-caroll-chief-editor.jpg",
+  },
+  {
+    nomeCognome: "Walter Gordon",
+    ruolo: "Office Manager",
+    foto: "walter-gordon-office-manager.jpg",
+  },
+  {
+    nomeCognome: "Angela Lopez",
+    ruolo: "Social Media Manager",
+    foto: "angela-lopez-social-media-manager.jpg",
+  },
+  {
+    nomeCognome: "Scott Estrada",
+    ruolo: "Developer",
+    foto: "scott-estrada-developer.jpg",
+  },
+  {
+    nomeCognome: "Barbara Ramos",
+    ruolo: "Graphic Designer",
+    foto: "barbara-ramos-graphic-designer.jpg",
+  },
+];
